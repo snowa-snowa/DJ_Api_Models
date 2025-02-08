5783e9bf4e2954aa74b5f8c883c3b2524db68d20
snowa-snowa <sione8172635@gmail.com>
2025-02-08 15:10:00 +0500

    Api for models?

--- DJ_Api_Models/wsgi.py
+++ DJ_Api_Models/wsgi.py
@@ -7,10 +7,20 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
-import os
+# import os
+#
+# from django.core.wsgi import get_wsgi_application
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DJ_Api_Models.settings')
+#
+# application = get_wsgi_application()
 
+import os
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DJ_Api_Models.settings')
+path = '/home/snowa/test/main.py/snow'
+sys.path.append(path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'DJ_Api_Models.settings'
 
 application = get_wsgi_application()
